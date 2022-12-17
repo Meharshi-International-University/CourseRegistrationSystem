4efbd3d21909c5ec4d23294f5f2af66b4f5d5cd8
lily <lwamfes@gmail.com>
2022-12-17 01:24:23 -0600

    first commit

---
+++ src/test/java/courseRegistrationSystem/CourseRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package courseRegistrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseRegistrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
